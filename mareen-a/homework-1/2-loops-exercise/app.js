945e4a7ef76057fdfb204e5f7bf005e800848d94
mareen <mareen@dev.ngo>
2021-11-28 17:47:24 +0000

    2 loops exercise

--- mareen-a/homework-1/2-loops-exercise/app.js
+++ mareen-a/homework-1/2-loops-exercise/app.js
@@ -11,11 +11,17 @@
  * 2 that you want to calculate).
  **/
 
+let exponent = 10;
+
 /**
  * Step 2
  * Create a `for` loop that iterates starting at 1 and ending at the value of
  * the `exponent` variable.
  **/
+
+ for(let i = 1; i <= exponent; i++) {
+  console.log(i);
+ }
 
 /**
  * Step 3
@@ -24,6 +30,14 @@
  *   (Hint: https://mdn.io/pow)
  * - log the result to the console
  **/
+
+ Math.pow(7, 2);    // 49 7x7
+ Math.pow(7, 3);    // 343 7 x 7 x 7
+
+for(let i = 1; i <= exponent; i++) {
+  let answer =  Math.pow(i, 2); 
+  console.log(answer);
+}
 
 /**
  * Step 4
@@ -35,18 +49,57 @@
  *    first statement logged to the console would be '2 to the power of 1 = 2')
  **/
 
+ for(let i = 1; i <= exponent; i++) {
+  console.log(`Calculating 2 to the power of ${exponent}`);
+  let answer =  Math.pow(2, i); 
+  console.log(`2 to the power of ${i} = ${answer}`);
+}
+
 /**
  * BONUS 1: Rewrite your code to allow a user to enter the exponent value,
  * rather than hard-coding it into your program. (Hint: Read up on the
  * `window.prompt` method at https://mdn.io/prompt.)
  **/
 
+const addNumber = Number(window.prompt("Type a number"));
+let exponentTwo = addNumber;
+
+for(let i = 1; i <= exponentTwo; i++) {
+ console.log(`Calculating 2 to the power of ${exponentTwo}`);
+ let answer =  Math.pow(2, i); 
+ console.log(`2 to the power of ${i} = ${answer}`);
+}
+
 /**
  * BONUS 2: Rewrite your code to use a `while` loop rather than a `for` loop.
  * (https://mdn.io/while)
  **/
+ console.log('WHILE LOOP!')
+ const addNumber2 = Number(window.prompt("Type a number (while loop)"));
+ let exponentThree = addNumber2;
+ let baseNumber = 1;
+
+ while(baseNumber <= exponentThree) {
+  console.log(`Calculating 2 to the power of ${exponentThree}`);
+  let iterator = baseNumber++;
+  let answerTwo =  Math.pow(2, iterator); 
+  console.log(`2 to the power of ${iterator} = ${answerTwo}`);
+ }
+ 
 
 /**
  * BONUS 3: Rewrite your code to use a `do/while` loop rather than a `for` loop
  * or `while` loop. (https://mdn.io/do...while)
  **/
+ console.log('DO WHILE LOOP!')
+ const addNumber3 = Number(window.prompt("Type a number (do...while loop)"));
+ let exponentFour = addNumber3;
+ let iter = 1;
+ 
+ do {
+  console.log(`Calculating 2 to the power of ${exponentFour}`);
+  answerThree = Math.pow(2, iter); 
+  console.log(`2 to the power of ${iter} = ${answerThree}`);
+  iter = iter + 1;
+ } while (iter <= exponentFour);
+ 
