d2d42b8e1583ca394bea8dcaf8a4c94e8051719b
Abderahim ABBAS <abderahim.abbas@gmail.com>
2021-11-28 19:11:41 +0100

    1-loops-codealong

--- abderahim-abbas/homework/arrays-loops/1-loops-codealong/app.js
+++ abderahim-abbas/homework/arrays-loops/1-loops-codealong/app.js
@@ -1,35 +1,60 @@
 console.log('working!');
 
 const numbersNames = ['one', 'two', 'three', 'four', 'five'];
-
+console.log(numbersNames)
 const thingsInMyFridge = ['pizza', 'eggs', 'milk', 'beers', 'ketchup'];
+console.log(thingsInMyFridge)
 
 /**
  * .push
  */
-// code here
+console.log("The push() method adds one or more elements to the end of an array and returns the new length of the array.")
+let numbersNames1 = numbersNames.slice() //take copy of numbersNames
+let number = 'six'
+numbersNames1.push(number)
+console.log("Push ", number, "on numbersNames:", numbersNames1)
+let count = numbersNames1.push('seven')
+console.log("Push return the new length of array", count)
 
 /**
  * .unshift
  */
-// code here
+
+console.log("The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.")
+number = 'zero'
+console.log(`Unshift ${number} on numberNames: ${numbersNames1.unshift(number)}`)
+console.log(numbersNames1)
 
 /**
  * .pop
  */
-// code here
+console.log("The pop() method removes the last element from an array and returns that element. This method changes the length of the array.")
+numbersNames1.pop()
+console.log(`First Pop on numbersNames: ${numbersNames1}`)
+numbersNames1.pop()
+console.log(`Second Pop on numbersNames: ${numbersNames1}`)
 
 /**
  * .shift
  */
-// code here
+
+console.log("The shift() method removes the first element from an array and returns that removed element. This method changes the length of the array.")
+const removed = numbersNames1.shift()
+console.log("Removed number:", removed)
+console.log(numbersNames1)
 
 /**
  * .splice
  */
-// code here
+
+console.log("The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. To access part of an array without modifying it, see slice().")
+numbersNames1.splice(0,0,'zero')
+console.log(numbersNames1)
 
 /**
  * .for
  */
-// code here
+console.log("Given that for...in is built for iterating object properties, not recommended for use with arrays.")
+for (const key in numbersNames) {
+    console.log(`${key} => ${numbersNames[key]}`)
+}
