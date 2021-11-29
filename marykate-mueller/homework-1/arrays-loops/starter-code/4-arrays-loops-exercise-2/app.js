f39f29ad384192c98c4730137a3aabeb6693a29a
MK Mueller <marykate@movement.vote>
2021-11-29 13:31:35 -0500

    homework sunday 11/28

---
+++ marykate-mueller/homework-1/arrays-loops/starter-code/4-arrays-loops-exercise-2/app.js
@@ -0,0 +1,71 @@
+/*
+You're adding functionality to a basic website shopping cart. Starting with an
+array of prices for the items a user wants to buy, you need to calculate the
+price of each item with tax, and then calculate the cart total including tax.
+The cart and salesTax variables are already defined for you below.
+*/
+
+//The cart variable stores the prices for 3 products selected from an online store.
+let cart = [37.49, 8.99, 27.95];
+
+// The salesTax variable stores the local sales tax rate.
+let salesTax = 0.0875;
+
+console.log('Cart contents: ' + cart);
+
+// Complete the following:
+
+/*
+ 1. Create a `cartWithTax` variable with an empty array as its value. 
+*/
+let carWithTax = ['', '', ''];
+console.log('Question 1: ', carWithTax);
+
+/*
+ 2. Write a `forEach` loop that calculates the sales tax for each item in the cart 
+ array, adds that to the original price of the item, and stores the result in the 
+ cartWithTax array.
+*/
+
+let SalesTax = [Price * 0.07];
+let Price = [80000];
+let Cart = [SalesTax + Price];
+carWithTax.forEach(Price * SalesTax);
+console.log('Step 2: ', exponent, SalesTax, Price);
+
+/*
+ 3. Log the value of cartWithTax to the console after it includes all values.
+ Be sure to label your output in the console (remember: concatenation!)
+*/
+
+/*
+ 4. Create a cartTotalWithTax variable and set its value to 0. 
+*/
+var cartTotalWithTax
+var=0;
+
+console.log('Step 3', carTotalWithTax)
+/*
+ 5. Write a `for` loop to total the values in the cartWithTax array, and assign
+ the total as the value of the cartTotalWithTax variable. 
+*/
+
+/*
+ 6. Log the cartTotalWithTax variable to the console. Be sure to label your 
+ output in the console.
+*/
+
+// BONUS
+
+/*
+ 7. Update your code in Step 2 to round each item price with tax to the
+ nearest penny. (See https://mdn.io/round)
+*/
+
+Math.round(carTotalWithTax);
+console.log(Math.round(carTotalWithTax));
+
+/*
+ 8. Rewrite your code from Steps 1 & 2 using the `array.map()` method. 
+ (See https://mdn.io/map)
+*/
---
+++ marykate-mueller/readme.md
@@ -0,0 +1,11 @@
+# MK Mueller
+
+## Data + Systems Director in the Progressive Movement Space, Movement Voter Project
+
+I love building tools, systems and operationalizing grantmaking for progressive grassroots organizations across the US.
+
+## Why I am doing this course
+
+I am doing this because ...
+
+- I think JavaScript will improve my database / systems/ workflow building
