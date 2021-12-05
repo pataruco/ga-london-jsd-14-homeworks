28cf1da702fc56b8e0e6642a971ff98c004b8a5e
Abderahim ABBAS <abderahim.abbas@gmail.com>
2021-12-05 23:00:30 +0100

    99-bottles-of-beer

--- abderahim-abbas/homework-2/99-bottles-of-beer/index.js
+++ abderahim-abbas/homework-2/99-bottles-of-beer/index.js
@@ -15,4 +15,12 @@
   read up on if/else conditional statements at https://mdn.io/if...else).
 */
 
-'use strict';
+"use strict";
+let number_bottles = 0;
+const final_value = 100;
+for (let i = 0; i < 101; i++) {
+    number_bottles = final_value - i;
+    number_bottles === 1 ? console.log(`${number_bottles} bottle of Beer on the wall, ${number_bottles} bottles of beer.\n Take one down and pass it around, no more bottles of beer on the wall.`) : console.log(`${number_bottles} bottle of Beer on the wall, ${number_bottles} bottles of beer.\n Take one down and pass it around, ${number_bottles - 1} bottles of beer on the wall.`);
+    number_bottles === 0 ? console.log("\n No more bottles of beer on the wall, no more bottles of beer.\n Go to the store and buy some more, 99 bottles of beer on the wall.") : console.log("");
+    // console.log(`${number_bottles} bottles of Beer on the wall`);
+}
--- abderahim-abbas/objects-json/starter-code/2-objects-intro/app.js
+++ abderahim-abbas/objects-json/starter-code/2-objects-intro/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // 1. Create an object called car
 
@@ -15,32 +15,42 @@ car.fuellevel = 10;
 // 4. Add a method called go. When car.go is invoked, it should console.log('Here we go!')
 
 car.go = function () {
-    console.log('Here we go!');
-}
+    console.log("Here we go!");
+};
 
 // 5. Add a method called accelerate. When car.acceletate is invoked, the current speed should increase by 1 and fuel level should decrease by 1.
 
 car.accelerate = function () {
     car.speed = car.speed++;
     car.fuellevel = car.fuellevel--;
-}
+};
 
 // 6. Add a method called decelerate. When car.decelerate is invoked, the current speed should decrease by 1.
 
 car.decelerate = function (speed) {
     speed = speed--;
-}
-car.decelerate(10)
+};
+car.decelerate(10);
 // 7. Add a method called refuel. When car.refuel is invoked, the fuel level should go back to 10.
 
 car.refuel = function () {
     car.fuellevel = 10;
-}
+};
 
 // BONUS adapt the code to console log a warning if the fuel level drops below 2.
 
 car.fuelwarning = function () {
-    return car.fuellevel < 2 ? console.log('Warning'): console.log('Fuel level safe');
-}
+    return car.fuellevel < 2 ? console.log("Warning") : console.log("Fuel level safe");
+};
 
-car.fuelwarning()
+car.fuelwarning();
+
+const user = {
+    name: "abbas",
+    age: 27,
+};
+let key = "name";
+
+console.log(user[key]);
+console.log(user["name"])
+console.log(user.name);
