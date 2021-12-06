da59d00c95ffc3b782cc524cc4104d71a6913136
mareen <mareen@dev.ngo>
2021-12-06 22:06:54 +0000

    homework 2 complete

--- mareen-a/homework-2/random-address-generator/index.js
+++ mareen-a/homework-2/random-address-generator/index.js
@@ -7,7 +7,14 @@
   Step 1:
     Create arrays that contain dummy data for each of the following:
     door number, street name, city name, and post code
+*/
 
+ let doorNumber = ['24', '2', '17', '467']
+ let streetName = ['Kingsbury', 'Little Oaks', 'Davey', 'Avenue'];
+ let cityName = ['Birmingham', 'Nottingham', 'London', 'Leicester'];
+ let postCode = ['B20', 'B24', 'B36', 'L20'];
+
+/*
   Step 2:
     Write code that randomly selects one item from each of these arrays, uses
     the selected values to construct a string containing a random address, and
@@ -15,6 +22,17 @@
 
     HINT: You will need to use methods in JavaScript's Math object
     (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math)
+
+*/
+
+
+const randomNumber = Math.floor(Math.random() * doorNumber.length);
+const randomStreet = Math.floor(Math.random() * streetName.length);
+const randomCity = Math.floor(Math.random() * cityName.length);
+const randomCode = Math.floor(Math.random() * postCode.length);
+console.log(`${doorNumber[randomNumber]} ${streetName[randomStreet]}, ${cityName[randomCity]}, ${postCode[randomCode]}`);
+
+/*
 
   Step 3:
     Save your work, run the program in Node, and verify
